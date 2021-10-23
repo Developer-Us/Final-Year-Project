591a4c746dbc873fe9b27602ae222aba4251b08b
Madhukrishna <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2021-10-23 12:10:34 +0530

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -6,16 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Developer-Us"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to Developer.Us Community
         </a>
       </header>
     </div>
