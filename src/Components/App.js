822cedd13a292858415c15fbf757e9e3469f5fe1
Madhukrishna <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2022-02-04 13:18:03 +0530

    Initial Setup - [Refactored the default React app]

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import '../Styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div> Vedant - make navbar.js 
+    </div>
+    <div> mk & yd  - make body.js and inner components</div>
+    </div>
+  );
+}
+
+export default App;
