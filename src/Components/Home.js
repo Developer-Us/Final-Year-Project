7c8e119c3e127d54317bb23f30f6a8b3970fc578
Madhukrishna <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2022-02-06 11:42:28 +0530

    refactored Yadnesh's VideoSection

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
-//import VedioContainer from './VedioContainer' Vedio container made by Yadhnesh...
+import VedioContainer from './VedioContainer' 
+// Vedio container made by Yadhnesh...
 export default function Home() {
   return (
   <>
-  {/*<VedioContainer/> Vedio container made by Yadhnesh...*/}
+  <VedioContainer/> 
   </>
   );
 }
