fc696fc0dbdd07b5b506c012f911465b7f7ea9f3
Madhukrishna <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2022-02-06 15:01:33 +0530

    Update VedioScreen.js

--- src/Components/VedioScreen.js
+++ src/Components/VedioScreen.js
@@ -9,7 +9,7 @@ const VedioScreen=()=>{
             <div id="VedioDetail">
             <span id="logo"><img src="Images/VedioThumnail.jpg" style={{borderRadius:"50%"}}height="40px" width="40px" alt=".." /></span>
             <ul>
-            <li><p style={{width:"250px",height:"24px",overflow:"hidden"}}>Website making in 40 minutes</p></li>
+            <li><h4 style={{width:"250px",height:"24px",overflow:"hidden"}}>Website making in 40 minutes</h4></li>
             <li>Technical Fande</li>
             <li>45k views . 2 years ago</li>
             </ul>
