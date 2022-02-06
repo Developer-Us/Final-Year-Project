9922f01b14230d22ad5433d4866a2807495b6451
Amit <amitsaliap@gmail.com>
2022-02-06 20:57:06 +0530

    created a rectangle added user Avatar from material UI

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Grid,Paper,Avatar } from '@material-ui/core';
 
 export default function login() {
+
+  const paperStyle={padding :20, height:'70vh', width:280, margin:"20px auto"}
   return <div>
-    <h1>this is log in page</h1>
+    <Grid>
+      <Paper elevation={10} style={paperStyle}>
+      <Avatar>h</Avatar>  
+      Sign in
+      </Paper>
+    </Grid>
+  
 
       
   </div>;
