d27e73a9f78b90a9de1f2f4df2893545207d94ea
Amit <amitsaliap@gmail.com>
2022-02-07 14:54:44 +0530

    login is page is ready made template i took from internet,it was in pure html, vanilla css, when i imported all code in react, because it was builed on pure html/css code were not optimised according to react, fixed all errors, give some touchup code

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,60 +1,70 @@
 import React from "react";
-import { Grid, Paper, Avatar,TextField, Button, Typography, Link } from "@material-ui/core";
-import Checkbox from '@mui/material/Checkbox';
-import  FormControlLabel  from "@material-ui/core/FormControlLabel";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import '../Styles/Login.css';
+import '../Styles/LoginUtil.css';
 
 export default function login() {
-  const btnstyle={margin:'8px 0'}
-  const paperStyle = {
-    padding: 20,
-    height: "70vh",
-    width: 280,
-    margin: "20px auto",
-  };
+
   return (
-    <div>
-      <Grid>
-        <Paper elevation={10} style={paperStyle}>
+    <div class="container-login100" >
+        <div class="wrap-login100 p-l-110 p-r-110 p-t-62 p-b-33">
+            <form class="login100-form validate-form flex-sb flex-w">
+                <span class="login100-form-title p-b-53">
+                    Sign In With
+                </span>
 
-          <Grid align="center">
-            <Avatar>
-              <LockOutlinedIcon />
-            </Avatar>
-            <h2>Sign in</h2>
-          </Grid>
+                <a href="/" class="btn-face m-b-20">
+                    <i class="fa fa-facebook-official"></i>
+                    Facebook
+                </a>
 
-          <TextField id="standard-basic" label="Mail" variant="standard" fullWidth required />
-          
-          <TextField id="standard-basic" label="Password" variant="standard" type="password" fullWidth required  />
+                <a href="/" class="btn-google m-b-20">
+                    <img src="Images/icon-google.png" alt="GOOGLE"/>
+                    Google
+                </a>
+                
+                <div class="p-t-31 p-b-9">
+                    <span class="txt1">
+                        Username
+                    </span>
+                </div>
+                <div class="wrap-input100 validate-input" data-validate = "Username is required">
+                    <input class="input100" type="text" name="username" />
+                    <span class="focus-input100"></span>
+                </div>
+                
+                <div class="p-t-13 p-b-9">
+                    <span class="txt1">
+                        Password
+                    </span>
 
-          <FormControlLabel
-            control={
-              <Checkbox
-              
-              name="checkedB"
-              color="primary"
-              />
-            }
-            label="Remember password"
+                    <a href="/" class="txt2 bo1 m-l-5">
+                        Forgot?
+                    </a>
+                </div>
+                <div class="wrap-input100 validate-input" data-validate = "Password is required">
+                    <input class="input100" type="password" name="pass" />
+                    <span class="focus-input100"></span>
+                </div>
 
-          />
-        
-        <Button type='submit' color='primary' fullWidth variant="contained" style={btnstyle}>Sign in</Button>
-          <Typography>
-            <Link href="#"  >
-              Forgot password
-            </Link>
-          </Typography>
+                <div class="container-login100-form-btn m-t-17">
+                    <button class="login100-form-btn">
+                        Sign In
+                    </button>
+                </div>
 
-          <Typography>
-            <Link href="#"  >
-              Sign Up
-            </Link>
-          </Typography>
+                <div class="w-full text-center p-t-55">
+                    <span class="txt2">
+                        Not a member?
+                    </span>
 
-        </Paper>
-      </Grid>
+                    <a href="/" class="txt2 bo1">
+                        Sign up now
+                    </a>
+                </div>
+            </form>
+        </div>
     </div>
+
+   
   );
 }
