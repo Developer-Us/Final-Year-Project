819ee694db1849b0027a80daae44edb7bb5222ce
Madhukrishna <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2022-02-18 20:55:29 +0530

    Added VideoWatchSection Component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,14 +3,16 @@ import LeftBar from './LeftBar';
 import Navbar from './Navbar';
 import VedioContainer from './VedioContainer';
 import Login from './Login'
+import VideoWatchSection from './VideoWatchSection';
 // Vedio container made by Yadhnesh...
 export default function Home() {
   return (
     <>
       <Navbar/>
-      <LeftBar/>
+      {/* <LeftBar/>
       <VedioContainer/>
-    <Login/>
+    <Login/> */}
+    <VideoWatchSection/>
     </>
   );
 }
---
+++ src/Components/VideoWatchSection.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import '../Styles/VideoWatchSection.css';
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
+import ReplyIcon from '@mui/icons-material/Reply';
+import WatchLaterIcon from '@mui/icons-material/WatchLater';
+import ReportIcon from '@mui/icons-material/Report';
+
+export default function VideoWatchSection() {
+
+  //Functions for handling hover effect for like, share ...btns
+  const DisplayLikeText = ()=>{
+    document.getElementById("likeBtnText").style.display="block";
+  }
+  const HideLikeText = ()=>{
+    document.getElementById("likeBtnText").style.display="none";
+  }
+
+  const DisplayShareText = ()=>{
+    document.getElementById("shareBtnText").style.display="block";
+  }
+  const HideShareText = ()=>{
+    document.getElementById("shareBtnText").style.display="none";
+  }
+
+  const DisplayWatchLaterText = ()=>{
+    document.getElementById("wlBtnText").style.display="block";
+  }
+  const HideWatchLaterText= ()=>{
+    document.getElementById("wlBtnText").style.display="none";
+  }
+
+  const DisplayReportText = ()=>{
+    document.getElementById("reportBtnText").style.display="block";
+  }
+  const HideReportText= ()=>{
+    document.getElementById("reportBtnText").style.display="none";
+  }
+  return (
+    <div className="VideoWatchSection">
+      <div className='VideoPlaySection'>
+        <div className="VideoPlayer">
+          <div className="Video">
+            <video className="VideoTag" src="Sample Video/sampleVideo.mp4" type="video/mp4" controls>
+            </video>
+          </div>
+
+          <div className='VideoMetaData'>
+            <div className="VideoTitle">
+              Piyush Bansal's Lenskart's Journey - Shark Tank India | Madhukrishna Nipankar overtook piyush | Yadhnesh starts laughing
+            </div>
+            
+            <div className="LikeShareWatchLaterReportBtns">
+              <span style={{ display: "flex", flexDirection: "column" }}><ThumbUpAltIcon onMouseEnter={DisplayLikeText} onMouseLeave={HideLikeText} className="VideoMetaBtn" style={{ height: "35px", width: "35px", margin: "5px 10px" }} /><p id="likeBtnText">Like</p></span>
+              <span style={{ display: "flex", flexDirection: "column" }}><ReplyIcon onMouseEnter={DisplayShareText} onMouseLeave={HideShareText} className="VideoMetaBtn" style={{ height: "35px", width: "35px", margin: "5px 10px" }} /><p id="shareBtnText">Share</p></span>
+              <span style={{ display: "flex", flexDirection: "column" }}><WatchLaterIcon onMouseEnter={DisplayWatchLaterText} onMouseLeave={HideWatchLaterText} className="VideoMetaBtn" style={{ height: "35px", width: "35px", margin: "5px 10px" }} /> <p id="wlBtnText">watch later</p></span>
+              <span style={{ display: "flex", flexDirection: "column" }}><ReportIcon onMouseEnter={DisplayReportText} onMouseLeave={HideReportText} className="VideoMetaBtn" style={{ height: "35px", width: "35px", margin: "5px 10px" }} /><p id="reportBtnText">Report</p></span>
+            </div>
+          </div>
+
+          <div className="ChannelInfo">
+              <div className="ChannelPic"><img src="Images/lenskart.jpg"  style={{borderRadius:"50%"}} height="50px" width="50px" alt=".." /></div>
+              <div className="ChannelName">Lenskart</div>
+          </div>
+        </div>
+
+        <div className="QuizSection">
+          Quiz Section
+        </div>
+      </div>
+    </div>
+  )
+}
