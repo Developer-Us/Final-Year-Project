a3b4b2bbc53c4ac960f07a4f0631a8e8507b91a2
Madhukrishna <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2022-02-18 18:56:53 +0530

    Update App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,15 +2,11 @@
 import '../Styles/App.css';
 
 import Home from './Home';
-import Login from './Login';
-import SignUp from './SignUp';
 function App() {
 
   return (
     <div className="App">
     <Home/>
-    <Login/>
-        <SignUp/>
     </div>
     
   );
