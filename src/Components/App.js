0a12290f8aaba66b63aa2a839a64d7149b172ab5
Amit <amitsaliap@gmail.com>
2022-02-18 15:41:07 +0530

    Login page finishing code

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,6 +10,7 @@ function App() {
     <div className="App">
         <SignUp/>
     </div>
+    
   );
 }
 
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,42 +1,41 @@
 import React from "react";
-import '../Styles/Login.css';
-
+import "../Styles/Login.css";
 
 export default function Login() {
-
   return (
-   
     <>
-    <div className={'Login'}>
+      <div className={"Login"}>
         <form>
-            <h2>Log in</h2>
+          <h2>Log in</h2>
 
-            <p>
-                <label htmlFor="email" className="floatLabel">email</label>
-			    <input id="U_Name" name="Username" type="text"/>
-            </p>
-		  
-		    <p>
-			    <label htmlFor="password" className="floatLabel">Password</label>
-			    <input id="password" name="password" type="password"/>
-          <span>forgot password ? </span>
-			   
-		    </p>
-		   
-		    <p>
-			    <input type="button" value="Log in" id="submit"/>
-		    </p>
-            <p>
-            <button id='text'>
-            Log in with 
-            </button>
-            </p>
-            <p>
-                <span>Dont have account? <button> Sign up</button></span>
-            </p>
-	    </form>
-        </div>
+          <p>
+            <label htmlFor="email" className="floatLabel">
+              email
+            </label>
+            <input id="U_Name" name="Username" type="text" />
+          </p>
+
+          <p>
+            <label htmlFor="password" className="floatLabel">
+              Password
+            </label>
+            <input id="password" name="password" type="password" />
+            <span>forgot password ? </span>
+          </p>
+
+          <p>
+            <input type="button" value="Log in" id="submit" />
+          </p>
+          <p>
+            <button id="text">Log in with</button>
+          </p>
+          <p>
+            <span>
+              Dont have account? <button> Sign up</button>
+            </span>
+          </p>
+        </form>
+      </div>
     </>
-   
   );
 }
